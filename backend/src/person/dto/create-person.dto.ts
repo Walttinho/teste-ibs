69cae077dd80e25af7ade4c74ad8ca34c41dd128
Walttinho <walter.netto@live.com>
2024-03-23 20:59:44 -0300

    feat(dto): Add CreatePersonDto
        - Adds CreatePersonDto to define the structure of data transfer object (DTO) for creating persons.

---
+++ backend/src/person/dto/create-person.dto.ts
@@ -0,0 +1,51 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsDate,
+  IsEnum,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export enum SexEnum {
+  MALE = 'M',
+  FEMALE = 'F',
+  OTHER = 'O',
+}
+
+export class CreatePersonDto {
+  constructor(
+    Name: string,
+    Sex: SexEnum,
+    BirthDate: Date,
+    MaritalStatus: string,
+  ) {
+    this.Name = Name;
+    this.Sex = Sex;
+    this.BirthDate = BirthDate;
+    this.MaritalStatus = MaritalStatus;
+  }
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(500)
+  public Name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(SexEnum)
+  public Sex: SexEnum;
+
+  @IsNotEmpty()
+  @IsDate()
+  @Type(() => Date)
+  public BirthDate: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(500)
+  public MaritalStatus: string;
+}
