64e7dd7a348c259fbc6874eae7b43d9f958964f5
Walttinho <walter.netto@live.com>
2024-03-25 22:54:03 -0300

    feat(angular): Create Angular project and install Tailwind CSS
    - Created a new Angular project using the Angular CLI
    - Installed Tailwind CSS for styling purposes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
